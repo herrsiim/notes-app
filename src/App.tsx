b585bd9984e1822f2b338b62808d686df30fcf43
Herrsiim <herrsiim@gmail.com>
2023-08-01 01:23:36 +0300

    New routes and new form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Route, Routes } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Navigate, Route, Routes } from "react-router-dom"
+import { NewNote } from "./NewNote"
 
 function App() {
-	return <Routes>
-		<Route path="/" element={<h1>Hi</h1>}></Route>
-		<Route path="/new" element={<h1>New</h1>}></Route>
-	</Routes>
+	return (
+		<Container className="my-4">
+			<Routes>
+				<Route path="/" element={<h1>Hi</h1>}></Route>
+				<Route path="/new" element={<NewNote />}></Route>
+				<Route path="/:id">
+					<Route index element={<h1>Show</h1>} />
+					<Route path="edit" index element={<h1>Edit</h1>} />
+				</Route>
+				<Route path="/new" element={<h1>New</h1>}></Route>
+				<Route path="*" element={<Navigate to="/" />}></Route>
+			</Routes>
+		</Container>
+	)
 }
 
 export default App
