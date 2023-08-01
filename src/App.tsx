cf06c5814a887a2bd37c734e9b4d3a78d4ef94eb
Herrsiim <herrsiim@gmail.com>
2023-08-01 09:25:49 +0300

    useLocalStorage and form adjustments

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,54 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { Container } from "react-bootstrap"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { NewNote } from "./NewNote"
+import { useLocalStorage } from "./useLocalStorage"
+import { v4 as uuidV4 } from "uuid"
+import { useMemo } from "react"
+
+export type Note = {
+	id: string
+} & NoteData
+
+export type RawNote = {
+	id: string
+} & RawNoteData
+
+export type RawNoteData = {
+	title: string
+	markdown: string
+	tagIds: string[]
+}
+
+export type NoteData = {
+	title: string
+	markdown: string
+	tags: Tags[]
+}
+
+export type Tags = {
+	id: string
+	label: string
+}
 
 function App() {
+	const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+	const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return { ...note, tags: tags.filter(tag => note.tagIds.includes(tag.id)) }
+    })
+  }, [notes, tags])
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes(prevNotes => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map(tag => tag.id) },
+      ]
+    })
+  }
+
 	return (
 		<Container className="my-4">
 			<Routes>
